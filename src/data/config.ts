a065957b963f037652c1de609e5bd991a74a5dbf
David Mecorvin <david.mecorvin@hotmail.fr>
2026-04-29 18:55:45 +0000

    Use Jeremy hero photo

--- src/data/config.ts
+++ src/data/config.ts
@@ -222,7 +222,7 @@ export const portfolioConfig: PortfolioConfig = {
       },
     ],
     photo: {
-      src: "/hero-2.webp",
+      src: "/hero-jeremy.jpeg",
       alt: "Portrait de Jérémy-Morgan Patole",
     },
   },
